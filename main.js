5029ff9fb3b3dba1f3a4fc7497926f88c5f1d624
MABottiglieri <manuel.bottiglieri@gmail.com>
2019-06-12 16:10:33 +0200

    added process variable

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ require('electron-reload')(__dirname)
 let win
 
 function createWindow() {
-    win = new BrowserWindow({ width:800,height:600 })
+    win = new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences:{nodeIntegration:true}
+    })
     win.loadURL(url.format({
         pathname:path.join(__dirname,'main.html'),
         protocol:'file',
